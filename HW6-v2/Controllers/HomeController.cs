65e8973cf3ce2ab38a466f00c26e67451aa993df
21638684 <102986782+21638684@users.noreply.github.com>
2022-10-30 22:17:09 +0200

    Add project files.

---
+++ HW6-v2/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HW6_v2.Models;
+using PagedList.Mvc;
+using PagedList;
+
+namespace HW6_v2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Orders
+        private BikeStoresEntities db = new BikeStoresEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Orders()
+        {
+            List<OrdersViewModel> order = db.order_items.Select(p => new OrdersViewModel { OrderID = p.order_id, Category = p.product.category.category_name, ProdName = db.products.Where(x => x.product_id == p.product_id).FirstOrDefault(), Quantity = p.quantity, Price = p.list_price, Total = (p.list_price * p.quantity), Date = db.orders.Where(o => o.order_id == p.order_id).FirstOrDefault().order_date }).ToList();
+            return View(order);
+        }
+        public ActionResult Search(DateTime date)
+        {
+            string searchdate = date.ToShortDateString();
+            List<OrdersViewModel> order = db.order_items.Where(y => y.order.order_date <= date).Select(p => new OrdersViewModel { OrderID = p.order_id, Category = p.product.category.category_name, ProdName = db.products.Where(x => x.product_id == p.product_id).FirstOrDefault(), Quantity = p.quantity, Price = p.list_price, Total = (p.list_price * p.quantity), Date = db.orders.Where(o => o.order_id == p.order_id).FirstOrDefault().order_date }).ToList();
+            return View("Orders", order);
+        }
+        public ActionResult Paging(int? page)
+        {
+            List<OrdersViewModel> order = db.order_items.Select(p => new OrdersViewModel { OrderID = p.order_id, Category = p.product.category.category_name, ProdName = db.products.Where(x => x.product_id == p.product_id).FirstOrDefault(), Quantity = p.quantity, Price = p.list_price, Total = (p.list_price * p.quantity), Date = db.orders.Where(o => o.order_id == p.order_id).FirstOrDefault().order_date }).ToList();
+
+            return View("Orders", db.order_items.OrderBy(o => o.item_id).ToPagedList(page ?? 1, 10));
+        }
+             
+        public ActionResult Reports()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ HW6-v2/Models/OrdersViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW6_v2.Models
+{
+    public class OrdersViewModel
+    {
+            public int OrderID { get; set; }
+            public product ProdName { get; set; }
+            public decimal Price { get; set; }
+            public DateTime Date { get; set; }
+            public string Category { get; set; }
+            public int Quantity { get; set; }
+            public decimal Total { get; set; }
+
+
+    }
+}
